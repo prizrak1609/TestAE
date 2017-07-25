879ebcf8843885026e6dc508843f76fba6cd7bcc
Dima <gubatenkod@gmail.com>
2017-07-25 14:46:49 +0300

    add unit tests

--- TestAETests/TestAETests.swift
+++ TestAETests/TestAETests.swift
@@ -7,16 +7,27 @@
 //
 
 import XCTest
+import CoreLocation
+import GoogleMaps
 @testable import TestAE
 
 class TestAETests: XCTestCase {
-    
+
+    // Kiev
+    let startPlace = PlaceModel(name: "Kiev", latitude: 50.45, longitude: 30.52)
+    // Odessa
+    let endPlace = PlaceModel(name: "Odessa", latitude: 46.48, longitude: 30.72)
+
+    var server: Server?
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        server = Server()
     }
     
     override func tearDown() {
+        server = nil
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
@@ -24,13 +35,52 @@ class TestAETests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(true)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testServerGetRouteFromRouteModel() {
+        let routeModel = RouteModel(startPlace: startPlace, endPlace: endPlace)
+        var models = [GMSPolyline]()
+        let expecatation = expectation(description: #function)
+        server?.getRoute(from: routeModel, { result in
+            switch result {
+                case .failure(let error): XCTAssertTrue(false, error.localizedDescription)
+                case .success(let _models):
+                    models = _models
+                    expecatation.fulfill()
+            }
+        })
+        waitForExpectations(timeout: 20, handler: nil)
+        XCTAssertFalse(models.isEmpty)
     }
-    
+
+    func testServerGetRouteFromPlacesModel() {
+        var models = [GMSPolyline]()
+        let expecatation = expectation(description: #function)
+        server?.getRoute(from: startPlace, to: endPlace, { result in
+            switch result {
+                case .failure(let error): XCTAssertTrue(false, error.localizedDescription)
+                case .success(let _models):
+                    models = _models
+                    expecatation.fulfill()
+            }
+        })
+        waitForExpectations(timeout: 20, handler: nil)
+        XCTAssertFalse(models.isEmpty)
+    }
+
+    func testServerGetRouteFromCLLocationCoordinate2DModel() {
+        var models = [GMSPolyline]()
+        let expecatation = expectation(description: #function)
+        server?.getRoute(from: startPlace.location, to: endPlace.location, { result in
+            switch result {
+                case .failure(let error): XCTAssertTrue(false, error.localizedDescription)
+                case .success(let _models):
+                    models = _models
+                    expecatation.fulfill()
+            }
+        })
+        waitForExpectations(timeout: 20, handler: nil)
+        XCTAssertFalse(models.isEmpty)
+    }
 }
